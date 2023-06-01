f5cb763208267d86bb061b18e7f08fd6d42ccea5
EddyBel <dante61918@gmail.com>
2023-05-31 21:33:07 -0600

    Modification of env.lua variables
    - The logic to obtain the system name was modified, because it did not recognize the operating system correctly, the previous logic always obtained windows system.
    - The logic that obtains the linux distribution was modified, an error was obtained when continuing the loop after closing the file, it was modified stopping the loop and closing the file at the end.

--- lua/config/env.lua
+++ lua/config/env.lua
@@ -9,9 +9,9 @@ env.MACOS = "Darwin"
 
 ---This variable stores the name of the operating system where neovim is running.
 ---Return OS string Returns the name of the operating system Windows, Linux or MacOS, if not identified returns Unknown
-env.OS = vim.fn.has("win32") and env.WINDOWS
-    or vim.fn.has("unix") and env.Linux
-    or vim.fn.has("mac") and env.MacOS
+env.OS = (vim.fn.has("win32") == 1) and env.WINDOWS
+    or (vim.fn.has("unix") == 1) and env.LINUX
+    or (vim.fn.has("mac") == 1) and env.MACOS
     or "Unknown"
 
 -- This code snippet gets the name of the Linux distribution in case you are using Linux
@@ -22,9 +22,8 @@ if env.OS == env.LINUX then
         for line in file:lines() do
             local name = line:match("NAME=\"(.-)\"")
             if name then
-                file:close()
-                ---Variable that stores the Linux distribution used
                 env.LINUX_DISTRIBUTION = name
+                break
             end
         end
         file:close()
@@ -36,9 +35,8 @@ if env.OS == env.LINUX then
         for line in file:lines() do
             local name = line:match("DISTRIB_ID=(.-)$")
             if name then
-                file:close()
-                ---Variable that stores the Linux distribution used
                 env.LINUX_DISTRIBUTION = name
+                break
             end
         end
         file:close()
@@ -46,7 +44,9 @@ if env.OS == env.LINUX then
 
     -- If both files failed, return "Linux" as default
     ---Variable that stores the Linux distribution used
-    env.LINUX_DISTRIBUTION = "Linux"
+    if not env.LINUX_DISTRIBUTION then
+        env.LINUX_DISTRIBUTION = "Linux"
+    end
 end
 
 _G.ENV = env
